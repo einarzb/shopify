d90e04a790ea5e0740035389caba73c4cafc6ded
einarzb <EinarBarzilay@EinarBarzilays-MacBook.local>
2017-02-23 16:26:17 +0200

    fixed total and counting items

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 // an array with all of our cart items
 var cart = [];
+var total = 0;
+
+$(".bin").
 
 var updateCart = function () {
   $(".cart-list").empty();
+  total = 0;
 
-  var itemCounter = {}; //declaring map object
+  // var itemCounter = {}; //declaring map object
 
   for (var i = 0; i < cart.length; i++) {
-      var mostFrequent = cart[i].name; //key var
-  
-      //if item exists in array
-     if(itemCounter[mostFrequent]) {
-        itemCounter[mostFrequent].counter++;
-      //if not - create an object that holds it and count it
-     }else{ itemCounter[mostFrequent] = {
-            name: mostFrequent,
-            price: cart[i].price,
-            counter: 1
-          }//end object
-         }//end else object isn't on map - create it   
-      }//end for loop 
-
-  //for in loop - for looping inside the new object we created    
-  for (var property in itemCounter) {
-    $(".cart-list").append("<p>" + itemCounter[property].name + "(" + itemCounter[property].counter + ")" + itemCounter[property].price + "$</p>");
+    $(".cart-list").append("<p>" + cart[i].name + " " +  "(" + cart[i].counter + ")" + " - " + cart[i].price + "$" + "class=bin" + "</p>");  
+    total += cart[i].price * cart[i].counter;
+    $(".total").text(total);
   }; //end of for in loop 
 };//end of function
 
 var cart = []; //storage array
 
 var addItem = function (item) {
-    cart.push(item);
+    var itemExists = false;
+    for (var i = 0; i < cart.length; i++) {
+      if (cart[i].name === item.name) {
+         cart[i].counter++;
+         itemExists = true;
+      } 
+    };
+    
+    //if it wasnt in a for loop than this would be else: 
+    if (!itemExists) {
+      cart.push(item);
+    }
+
     console.log(cart);
 }
 
 // TODO: Write a function that clears the cart ;-)
 var clearCart = function () {
   $(".cart-list").empty();
-    console.log("empty!");
+  $(".total").empty(); //clear html
+  total = 0; //clear backend
+  console.log("empty!");
 
  //splicing the array
   cart.splice(0, cart.length); //the array length equals as the number of items to be removed
@@ -53,7 +57,8 @@ $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
   var name = $(this).parent().parent().parent().find(".item").data().name;
   var price = $(this).parent().parent().parent().find(".item").data().price;
-  var item = {name:name, price:price};
+  var counter = 1;
+  var item = {name:name, price:price, counter:counter};
   console.log(item);
   addItem(item); //invoking
   updateCart(); //invoking
